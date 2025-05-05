389fc06a93c9ebbf16c34b7e2998aa3a6a4a8a0f
Tee Age <65055124+T334GE@users.noreply.github.com>
2025-05-05 13:33:15 +0200

    -tweaked buttons

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame as py, sys, time, random
+
+class Game:
+    def __init__(self):
+        py.init()
+        self.screen = py.display.set_mode((width, height))
+
+
+
+if __name__ == "__main__":
+    pass
