00f167f825c2b131524ff6c4443f8ed1ae80b73a
T334GE <teeageing@gmail.com>
2025-09-19 15:33:04 +0200

    changing back to pygame OOP approach basic game window

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from scripts.App import GameApp
+import pygame as py, sys
 
-class Main:
-    """Main entry point for the application."""
-    def __init__(self):
-        self.__app = GameApp()
+class Game:
+    def __init__(self, screen):
+        py.init()
+        self.__screen = screen
+        self.__screen_width = self.__screen.get_width()
+        self.__screen_height = self.__screen.get_height()
+        self.__font = py.font.Font("assets/fonts/JetBrainsMono-Regular.ttf", 60)
+        self.__running:bool = True
+
     @property
-    def app(self):
-        """Returns the GameApp instance."""
-        return self.__app
+    def is_running(self):
+        return self.__running
+    @is_running.setter
+    def is_running(self, status:bool):
+        self.__running = status
 
+    def run(self):
+        while self.__running:
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    self.__running = False
+                    py.quit()
+                    sys.exit()
+
+            py.display.update()
 
 if __name__ == "__main__":
-    Main().app.run()
+    Game(
+        screen=py.display.set_mode((1366,1024))
+    ).run()
