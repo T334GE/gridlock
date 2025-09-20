9b13b842e1fa3321be298900123ed8c4af261eb8
T334GE <teeageing@gmail.com>
2025-09-20 12:25:22 +0200

    placed buttons in container and implemented flexible positioning in case of window resizing

--- main.py
+++ main.py
@@ -8,23 +8,34 @@ class Game:
         self.__screen_height = self.__screen.get_height()
         self.__screen_size = self.__screen_width, self.__screen_height
         self.__background = py.Surface(self.__screen_size)
-        
+
+        self.__font = py.font.Font("assets/fonts/JetBrainsMono-Regular.ttf", 60)
         self.__manager = pygui.UIManager(self.__screen_size, theme_path="scripts/theme.json")
         self.__clock = py.time.Clock()
         self.__time_delta = None
-        
-        self.__font = py.font.Font("assets/fonts/JetBrainsMono-Regular.ttf", 60)
+
         self.__is_running:bool = True
+
+        self.__menu_container = pygui.elements.UIPanel(
+            py.Rect(-1, -1, 200, 500),
+            anchors={"center":"center"},
+            object_id="main_menu_container"
+        )
         self.__start_button = pygui.elements.UIButton(
-            py.Rect(0, 350, 75, -1),
+            py.Rect(-1, 50, 75, -1),
             text="start",
-            anchors={"centerx":"centerx"
-                     }
+            anchors={"centerx":"centerx",
+                     "bottom_target": "self.__exit_button"},
+            container=self.__menu_container,
+            object_id="start_btn"
         )
         self.__exit_button = pygui.elements.UIButton(
-            py.Rect(0, 400, 75, -1),
+            py.Rect(-1, 100, 75, -1),
             text="exit",
-            anchors={"centerx": "centerx"}
+            anchors={"centerx":"centerx",
+                     "top_target": "self.__start_button"},
+            container=self.__menu_container,
+            object_id="exit_btn"
         )
 
 
@@ -41,20 +52,21 @@ class Game:
         while self.__is_running:
             self.__time_delta = self.__clock.tick(60)/1000.0
             for event in py.event.get():
-                if event.type == py.QUIT:
-                    self.__is_running = False
-
-                if event.type == pygui.UI_BUTTON_PRESSED:
-                    if event.ui_element.text == "exit":
+                match event.type:
+                    case py.QUIT:
                         self.__is_running = False
 
-                if event.type == py.VIDEORESIZE:
-                    self.__screen = py.display.set_mode((event.w, event.h),py.RESIZABLE)
-                    self.__screen_width = event.w
-                    self.__screen_height = event.h
-                    
+                    case pygui.UI_BUTTON_PRESSED:
+                        if event.ui_element.text == "exit":
+                            self.__is_running = False
+
+                    case py.VIDEORESIZE:
+                        self.__screen = py.display.set_mode((event.w, event.h),py.RESIZABLE)
+                        self.__manager.set_window_resolution((event.w, event.h))
+                        self.__menu_container.update(self.__time_delta)
+
                 self.__manager.process_events(event)
-            
+
             self.__manager.update(self.__time_delta)
             self.__screen.fill((0,0,0))
             self.__manager.draw_ui(self.__screen)
